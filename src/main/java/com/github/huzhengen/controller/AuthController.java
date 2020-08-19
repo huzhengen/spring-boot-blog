c7eafc13f66c35e7401305b9496adf9e47f3d74b
hukai <huzhengen@163.com>
2020-08-19 15:53:31 +0800

    logout

--- src/main/java/com/github/huzhengen/controller/AuthController.java
+++ src/main/java/com/github/huzhengen/controller/AuthController.java
@@ -7,7 +7,6 @@ import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -36,9 +35,34 @@ public class AuthController {
         String userName = SecurityContextHolder.getContext().getAuthentication().getName();
         User loggedInUser = userService.getUserByUsername(userName);
         if (loggedInUser == null) {
-            return new Result("ok", "用户没有登录", false);
+            return new Result("fail", "用户没有登录", false);
         } else {
             return new Result("ok", null, true, loggedInUser);
+        }
+    }
+
+    @PostMapping("/auth/register")
+    @ResponseBody
+    public Result register(@RequestBody Map<String, String> usernameAndPassword) {
+        String username = usernameAndPassword.get("username");
+        String password = usernameAndPassword.get("password");
+
+        if (username == null || password == null) {
+            return new Result("fail", "用户名或密码不能为空", false);
+        }
+        if (username.length() < 1 || username.length() > 15) {
+            return new Result("fail", "用户名长度需大于1小于15", false);
+        }
+        if (password.length() < 6) {
+            return new Result("fail", "密码需大于6位", false);
+        }
+
+        User user = userService.getUserByUsername(username);
+        if (user == null) {
+            userService.save(username, password);
+            return new Result("ok", "注册成功", false);
+        } else {
+            return new Result("fail", "该用户已注册", false);
         }
     }
 
@@ -52,7 +76,7 @@ public class AuthController {
         try {
             userDetails = userService.loadUserByUsername(username);
         } catch (UsernameNotFoundException e) {
-            return new Result("fail", "该用户不存在", false);
+            return new Result("fail", "用户名不存在", false);
         }
 
         UsernamePasswordAuthenticationToken token =
@@ -62,10 +86,22 @@ public class AuthController {
             authenticationManager.authenticate(token);
             // Cookie
             SecurityContextHolder.getContext().setAuthentication(token);
-//            User loggedInUser = new User(1, "张三");
             return new Result("ok", "登录成功", true, userService.getUserByUsername(username));
         } catch (BadCredentialsException e) {
             return new Result("fail", "密码不正确", false);
+        }
+    }
+
+    @GetMapping("/auth/logout")
+    @ResponseBody
+    public Result logout() {
+        String userName = SecurityContextHolder.getContext().getAuthentication().getName();
+        User loggedInUser = userService.getUserByUsername(userName);
+        if (loggedInUser == null) {
+            return new Result("fail", "用户没有登录", false);
+        } else {
+            SecurityContextHolder.clearContext();
+            return new Result("ok", "注销成功", false);
         }
     }
 
