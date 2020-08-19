d21c757b98250cf3c19f52eae04da237faab6eae
hukai <huzhengen@163.com>
2020-08-19 16:58:39 +0800

    JsonIgnore

--- src/main/java/com/github/huzhengen/entity/User.java
+++ src/main/java/com/github/huzhengen/entity/User.java
@@ -1,10 +1,13 @@
 package com.github.huzhengen.entity;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
 import java.time.Instant;
 
 public class User {
     Integer id;
     String username;
+    @JsonIgnore
     String encryptedPassword;
     String avatar;
     Instant createdAt;
